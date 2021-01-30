ee9958e6efc7da83926bc29cb4041e8e7eb88458
Destiny Jones <destinyj8899@gmail.com>
2021-01-30 12:19:39 -0800

    last fmunfinished

---
+++ lastfm.js
@@ -0,0 +1,19 @@
+/// Set Up API for lastfm ///
+var APIKey="4768c3baac58c09f5faba20fb58257a9";
+var track="";
+var findTrack=$("#search-track");
+var searchButton = $("#search-button");
+var artist=$("#artist");
+var URLname=$("#URL")
+
+/// Keyword search for name
+
+///AJAX call
+$("#search-button").on("click",trackResults);
+function trackNames(trackResults){
+    let queryURL= "/2.0/?method=track.search&track=" + track + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+    console.log(response);

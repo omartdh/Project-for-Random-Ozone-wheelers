6670c6b978a8c0d64df7084aaebbf7f9c3733530
Destiny Jones <destinyj8899@gmail.com>
2021-01-30 14:26:27 -0800

    update

--- lastfm.js
+++ lastfm.js
@@ -1,6 +1,6 @@
 /// Set Up API for lastfm ///
 var APIKey="4768c3baac58c09f5faba20fb58257a9";
-var track="";
+var track=$("#trackName");
 var findTrack=$("#search-track");
 var searchButton = $("#search-button");
 var artist=$("#artist");
@@ -10,10 +10,29 @@ var URLname=$("#URL")
 
 ///AJAX call
 $("#search-button").on("click",trackResults);
-function trackNames(trackResults){
+function trackNames(trackResults){ }
     let queryURL= "/2.0/?method=track.search&track=" + track + "&APPID=" + APIKey;
     $.ajax({
         url:queryURL,
         method:"GET",
     }).then(function(response){
-    console.log(response);
+    console.log(response)
+    //Cache object
+       var cache = new LastFMCache();
+       //LastFM object
+       var lastfm = new LastFM({
+       });
+    //load name of track
+           lastfm.track.getInfo({track: trackName, limit: 10}, {success: function(data){
+    //render data of track name
+               $('name').html(
+                   $('#lastfmTemplateTrackInfo').render(data.name)
+               );
+    //load the artist's name
+               lastfm.song.getArtist({artist: trackArtist}, {success: function(data){
+                   // render the tracks using 'lastfmTemplateTracks' template
+                   $('#').html(
+                       $('#lastfmTemplateTracks').render(data.artist.name)
+                   );
+               }});
+    //           
